867e222f58f961262d15bc256fcb869d28cf8fb7
crispynoodlesoup <51384880+crispynoodlesoup@users.noreply.github.com>
2024-12-14 05:10:27 -0600

    oops i forgot to commmit anything for the last hour haha

--- app.js
+++ app.js
@@ -2,17 +2,42 @@ const guessesBox = document.querySelector(".guesses");
 
 let guesses = ["", "", "", "", "", ""];
 
-function populate() {
-  guesses.forEach((text) => {
-    const tryBox = document.createElement("div");
-    tryBox.className = "try-box";
+guesses.forEach((text) => {
+  const tryBox = document.createElement("div");
+  tryBox.className = "try-box";
 
-    const p = document.createElement("p");
-    p.innerText = text;
+  const p = document.createElement("p");
+  p.innerText = text;
 
-    tryBox.append(p);
-    guessesBox.append(tryBox);
-  });
-}
+  tryBox.append(p);
+  guessesBox.append(tryBox);
+});
 
-populate();
+const skip = document.querySelector(".skip");
+
+const speeds = [-1, 10, 5, 4, 3, 2];
+let stage = 0;
+
+skip.addEventListener("click", () => {
+  stage++;
+  if (stage >= speeds.length) stage = 0;
+});
+
+const play = document.querySelector(".play");
+const audio1 = document.querySelector(".aud");
+
+const bopNum = Math.floor(Math.random() * 201) + 1;
+
+let playing = false;
+
+play.addEventListener("click", () => {
+  if (playing === false) {
+    audio1.src = `bops/(${bopNum}).mp3`;
+    audio1.volume = 0.05;
+    audio1.playbackRate = speeds[stage];
+    audio1.play();
+  } else {
+    audio1.pause();
+  }
+  playing = !playing;
+});
